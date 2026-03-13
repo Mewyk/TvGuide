7805a713821a9aba1e3d27af2e8ac7e861b997e4
Mewyk <public@mewyk.com>
2026-03-13 08:55:15 -0700

    Remove Twitch Helix modules and services

--- Modules/AuthenticationModule.Log.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.Extensions.Logging;
-
-namespace TvGuide.Modules;
-
-internal static partial class AuthenticationModuleLog
-{
-    [LoggerMessage(EventId = 1300, Level = LogLevel.Information, Message = "Authentication token acquired, expires at: {Expiration}")]
-    public static partial void AuthenticationTokenAcquired(ILogger logger, DateTime expiration);
-}
--- Modules/UsersModule.Log.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.Extensions.Logging;
-
-namespace TvGuide.Modules;
-
-internal static partial class UsersModuleLog
-{
-    [LoggerMessage(EventId = 1400, Level = LogLevel.Error, Message = "Failed to get user info: {StatusCode} - {Content}")]
-    public static partial void FailedToGetUserInfo(ILogger logger, System.Net.HttpStatusCode statusCode, string content);
-}
